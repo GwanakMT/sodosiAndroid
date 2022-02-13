df85ec0482095c006911915df5fd4bc71a7ae57c
정민지 <s2018s38@e-mirim.hs.kr>
2022-02-13 23:12:36 +0900

    지도 뷰 사용을 위해 T MAP API 추가
    - libs/com.skt.Tmap_1.70.jar
    - API KEY는 보안상의 이유로 local.properties에 저장

--- app/src/main/java/com/github/sookhee/sodosi/MainActivity.kt
+++ app/src/main/java/com/github/sookhee/sodosi/MainActivity.kt
@@ -3,6 +3,7 @@ package com.github.sookhee.sodosi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.github.sookhee.sodosi.databinding.ActivityMainBinding
+import com.skt.Tmap.TMapView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -12,6 +13,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
+        val tmapview = TMapView(this);
+        tmapview.setSKTMapApiKey(TMAP_API_KEY)
+
+        binding.rootView.addView(tmapview)
+
         setContentView(binding.root)
+    }
+
+    companion object {
+        private val TMAP_API_KEY = BuildConfig.TMAP_API_KEY
     }
 }
