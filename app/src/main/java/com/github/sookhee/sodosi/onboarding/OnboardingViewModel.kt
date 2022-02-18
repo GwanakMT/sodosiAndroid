6707937769c8741a363bcdde459ea1140a23849f
정민지 <s2018s38@e-mirim.hs.kr>
2022-02-18 22:02:29 +0900

    Navigation Component로 온보딩 프로세스 구성
    - Step1: 시작
    - Step2: 주소 입력
    - Step3: 휴대폰번호 입력
    - Step4: 인증번호 입력
    - Step5: 비밀번호 입력
    - Step6: 닉네임 입력
    - 현재 기획 상태로 추후 프로세스 변경될 수 있음...

---
+++ app/src/main/java/com/github/sookhee/sodosi/onboarding/OnboardingViewModel.kt
@@ -0,0 +1,13 @@
+package com.github.sookhee.sodosi.onboarding
+
+import com.github.sookhee.sodosi.common.base.BaseViewModel
+
+/**
+ *  OnboardingViewModel.kt
+ *
+ *  Created by Minji Jeong on 2022/02/18
+ *  Copyright © 2022 DonorPets2.0. All rights reserved.
+ */
+class OnboardingViewModel: BaseViewModel() {
+
+}
