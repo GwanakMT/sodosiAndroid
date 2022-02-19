3ba2513b517a5f15a23e8f9ad13a02ff605f288d
정민지 <s2018s38@e-mirim.hs.kr>
2022-02-20 01:01:25 +0900

    메뉴 버튼 눌렀을 때 Bottom Menu Dialog 추가
    - 이 글 복사하기
    - 신고
    - 닫기(dismiss)

--- app/src/main/java/com/github/sookhee/sodosi/thread/ThreadActivity.kt
+++ app/src/main/java/com/github/sookhee/sodosi/thread/ThreadActivity.kt
@@ -1,10 +1,19 @@
 package com.github.sookhee.sodosi.thread
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.viewModels
+import com.github.sookhee.sodosi.R
 import com.github.sookhee.sodosi.common.base.BaseActivity
 import com.github.sookhee.sodosi.databinding.ActivityThreadBinding
 
 class ThreadActivity : BaseActivity<ThreadViewModel, ActivityThreadBinding>() {
+    private lateinit var menuDialog: Dialog
+
     override fun getViewBinding() = ActivityThreadBinding.inflate(layoutInflater)
 
     override val viewModel: ThreadViewModel by viewModels()
@@ -14,8 +23,38 @@ class ThreadActivity : BaseActivity<ThreadViewModel, ActivityThreadBinding>() {
     }
 
     override fun initViews() {
+        setOnClickListener()
+        initDialog()
+    }
+
+    private fun setOnClickListener() {
         binding.btnBack.setOnClickListener { onBackPressed() }
-        binding.btnMenu.setOnClickListener {  }
-        binding.btnShare.setOnClickListener {  }
+        binding.btnMenu.setOnClickListener { menuDialog.show() }
+        binding.btnShare.setOnClickListener { }
+    }
+
+    private fun initDialog() {
+        menuDialog = Dialog(this)
+        menuDialog.apply {
+            setContentView(R.layout.dialog_thread_menu)
+
+            findViewById<TextView>(R.id.tvCommentSave).setOnClickListener {
+                Toast.makeText(this@ThreadActivity, "이 글 저장하기", Toast.LENGTH_SHORT).show()
+            }
+
+            findViewById<TextView>(R.id.tvCommentReport).setOnClickListener {
+                Toast.makeText(this@ThreadActivity, "신고", Toast.LENGTH_SHORT).show()
+            }
+
+            findViewById<TextView>(R.id.tvDismiss).setOnClickListener {
+                menuDialog.dismiss()
+            }
+
+            window?.apply {
+                setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+                attributes.windowAnimations = R.style.BottomDialogAnimation
+            }
+        }
     }
 }
