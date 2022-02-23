673441f6a6592ae6287ea6fe69af7da6be95c9af
정민지 <s2018s38@e-mirim.hs.kr>
2022-02-23 18:12:34 +0900

    PermissionManager 변수에 const 키워드 붙여주기

--- app/src/main/java/com/sodosi/util/PermissionManager.kt
+++ app/src/main/java/com/sodosi/util/PermissionManager.kt
@@ -15,8 +15,8 @@ import androidx.core.app.ActivityCompat
  */
 
 object PermissionManager {
-    val REQUEST_CODE = 1000
-    val ACCESS_FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION
+    const val REQUEST_CODE = 1000
+    const val ACCESS_FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION
 
     fun hasPermission(context: Context, permission: String): Boolean {
         return ActivityCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
