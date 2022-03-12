bfe55aae225632f3197f5b2acce291a050fd8cac
정민지 <s2018s38@e-mirim.hs.kr>
2022-03-13 00:20:40 +0900

    온보딩 프로세스 확정됨에 따라 Fragment 네이밍 수정 (_PasswordFragment)
    - Step4Fragment -> SignPasswordFragment
    - Step4Fragment -> LoginPasswordFragment

---
+++ app/src/main/java/com/sodosi/ui/onboarding/SignPasswordFragment.kt
@@ -0,0 +1,43 @@
+package com.sodosi.ui.onboarding
+
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.sodosi.R
+import com.sodosi.databinding.FragmentSignPasswordBinding
+import com.sodosi.ui.common.base.BaseFragment
+
+/**
+ *  SignPasswordFragment.kt
+ *
+ *  Created by Minji Jeong on 2022/03/13
+ *  Copyright © 2022 GwanakMT All rights reserved.
+ */
+
+class SignPasswordFragment : BaseFragment<OnboardingViewModel, FragmentSignPasswordBinding>() {
+    override fun getViewBinding() = FragmentSignPasswordBinding.inflate(layoutInflater)
+
+    override val viewModel: OnboardingViewModel by viewModels()
+
+    override fun initViews() = with(binding) {
+        initAppbar()
+        setOnClickListener()
+    }
+
+    override fun observeData() {
+
+    }
+
+    private fun initAppbar() {
+        binding.appbar.apply {
+            initLeftButton(R.drawable.ic_arrow_left) {
+                activity?.onBackPressed()
+            }
+        }
+    }
+
+    private fun setOnClickListener() {
+        binding.btnNext.setOnClickListener {
+            findNavController().navigate(Step4FragmentDirections.actionFragmentStep4ToFragmentStep5())
+        }
+    }
+}
