705c14d70f055bb0a54225fbec757d99b10e7255
정민지 <s2018s38@e-mirim.hs.kr>
2022-04-18 23:40:46 +0900

    마이페이지 기본 UI 구성 (+ 임시 데이터)

--- app/src/main/java/com/sodosi/ui/mypage/MypageActivity.kt
+++ app/src/main/java/com/sodosi/ui/mypage/MypageActivity.kt
@@ -1,6 +1,7 @@
 package com.sodosi.ui.mypage
 
 import androidx.activity.viewModels
+import com.sodosi.R
 import com.sodosi.databinding.ActivityMypageBinding
 import com.sodosi.ui.common.base.BaseActivity
 
@@ -14,6 +15,27 @@ class MypageActivity : BaseActivity<MypageViewModel, ActivityMypageBinding>() {
     }
 
     override fun initViews() = with(binding) {
+        initAppbar()
+        initProfile()
+    }
 
+    private fun initAppbar() {
+        binding.appbar.apply {
+            initLeftButton(R.drawable.ic_arrow_left) {
+                onBackPressed()
+            }
+
+            initRightButton(R.drawable.ic_interface_settings_future) {
+
+            }
+        }
+    }
+
+    private fun initProfile() {
+        binding.tvProfileNickname.text = "중구구립도서관"
+        binding.tvHourAgo.text = "1시간 전 방문"
+        binding.tvCreatedSodosiCount.text = "35"
+        binding.tvJoinedSodosiCount.text = "35"
+        binding.tvBookmarkCount.text = "35"
     }
 }
