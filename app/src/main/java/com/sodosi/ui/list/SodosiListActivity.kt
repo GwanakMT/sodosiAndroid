7badb000897c756dd61dfd363fc277c22d340741
정민지 <s2018s38@e-mirim.hs.kr>
2022-09-09 12:01:45 +0900

    전체 소도시 리스트 화면 UI

--- app/src/main/java/com/sodosi/ui/list/SodosiListActivity.kt
+++ app/src/main/java/com/sodosi/ui/list/SodosiListActivity.kt
@@ -1,15 +1,70 @@
 package com.sodosi.ui.list
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.graphics.Color
+import android.graphics.Typeface
+import androidx.activity.viewModels
+import com.sodosi.R
 import com.sodosi.databinding.ActivitySodosiListBinding
+import com.sodosi.ui.common.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
-class SodosiListActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySodosiListBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivitySodosiListBinding.inflate(layoutInflater)
+@AndroidEntryPoint
+class SodosiListActivity : BaseActivity<SodosiListViewModel, ActivitySodosiListBinding>() {
+    override val viewModel: SodosiListViewModel by viewModels()
 
-        setContentView(binding.root)
+    override fun getViewBinding() = ActivitySodosiListBinding.inflate(layoutInflater)
+
+    override fun observeData() {
+
+    }
+
+    override fun initViews() = with(binding) {
+        initAppbar()
+        initRecyclerView()
+        initSortTab()
+
+        setOnClickListener()
+    }
+
+    private fun initAppbar() {
+        binding.appbar.apply {
+            initLeftButton(R.drawable.ic_arrow_left) {
+                onBackPressed()
+            }
+
+            initAppbarTitle(getString(R.string.sodosi_total_list_title))
+        }
+    }
+
+    private fun initRecyclerView() {
+
+    }
+
+    private fun initSortTab() {
+
+    }
+
+    private fun setOnClickListener() {
+        binding.tvSortPopular.setOnClickListener {
+            // selected
+            viewModel.sortBy()
+            binding.tvSortPopular.typeface = Typeface.DEFAULT_BOLD
+            binding.tvSortPopular.setTextColor(Color.parseColor("#0F0F10"))
+
+            // unselected
+            binding.tvSortFastest.typeface = Typeface.DEFAULT
+            binding.tvSortFastest.setTextColor(Color.parseColor("#8A8A8E"))
+        }
+
+        binding.tvSortFastest.setOnClickListener {
+            // selected
+            viewModel.sortBy()
+            binding.tvSortFastest.typeface = Typeface.DEFAULT_BOLD
+            binding.tvSortFastest.setTextColor(Color.parseColor("#0F0F10"))
+
+            // unselected
+            binding.tvSortPopular.typeface = Typeface.DEFAULT
+            binding.tvSortPopular.setTextColor(Color.parseColor("#8A8A8E"))
+        }
     }
 }
---
+++ app/src/main/java/com/sodosi/ui/list/SodosiListViewModel.kt
@@ -0,0 +1,21 @@
+package com.sodosi.ui.list
+
+import com.sodosi.ui.common.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+/**
+ *  SodosiListViewModel.kt
+ *
+ *  Created by Minji Jeong on 2022/09/09
+ *  Copyright © 2022 GwanakMT All rights reserved.
+ */
+
+@HiltViewModel
+class SodosiListViewModel @Inject constructor(
+
+) : BaseViewModel() {
+    fun sortBy() {
+        // TODO: 구현
+    }
+}
